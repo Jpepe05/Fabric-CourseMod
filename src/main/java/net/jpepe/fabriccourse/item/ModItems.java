ff4a7e1e0fd4ca8dee97472a171f26bff368d706
Joao Ribeiro <joaopedrocastro05@gmail.com>
2022-06-19 01:30:57 -0300

    Add advanced Item

--- src/main/java/net/jpepe/fabriccourse/item/ModItems.java
+++ src/main/java/net/jpepe/fabriccourse/item/ModItems.java
@@ -2,6 +2,7 @@ package net.jpepe.fabriccourse.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.jpepe.fabriccourse.MCCourseMod;
+import net.jpepe.fabriccourse.item.custom.DowsingRodItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -19,6 +20,11 @@ public class ModItems {
   public static final Item RAW_ORICHALCUM =
       registerItem(
           "raw_orichalcum", new Item(new FabricItemSettings().group(ModItemGroups.COURSE)));
+
+  public static final Item DOWSING_ROD =
+      registerItem(
+          "dowsing_rod",
+          new DowsingRodItem(new FabricItemSettings().group(ModItemGroups.COURSE).maxDamage(32)));
 
   private static Item registerItem(String name, Item item) {
     return Registry.register(Registry.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
