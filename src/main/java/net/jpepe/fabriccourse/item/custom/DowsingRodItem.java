06a1ed1854d10d901c23ee1fcf944944b1944619
Joao Ribeiro <joaopedrocastro05@gmail.com>
2022-06-19 17:40:02 -0300

    Add tags

--- src/main/java/net/jpepe/fabriccourse/item/custom/DowsingRodItem.java
+++ src/main/java/net/jpepe/fabriccourse/item/custom/DowsingRodItem.java
@@ -1,18 +1,37 @@
 package net.jpepe.fabriccourse.item.custom;
 
+import net.jpepe.fabriccourse.util.ModTags;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class DowsingRodItem extends Item {
   public DowsingRodItem(Settings settings) {
     super(settings);
+  }
+
+  @Override
+  public void appendTooltip(
+      ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+    if (Screen.hasShiftDown()) {
+      tooltip.add(new TranslatableText("item.mccourse.downsing_rod.tooltip.shift"));
+    } else {
+      tooltip.add(new TranslatableText("item.mccourse.downsing_rod.tooltip"));
+    }
   }
 
   @Override
@@ -23,9 +42,10 @@ public class DowsingRodItem extends Item {
       boolean foundBlock = false;
 
       for (int i = 0; i <= positionClicked.getY(); i++) {
-        Block blockBelow = context.getWorld().getBlockState(positionClicked.down(i)).getBlock();
+        BlockState stateBelow = context.getWorld().getBlockState(positionClicked.down(i));
+        Block blockBelow = stateBelow.getBlock();
 
-        if (isValuableBlock(blockBelow)) {
+        if (isValuableBlock(stateBelow)) {
           outputValuableCoordinates(
               positionClicked, player, blockBelow, positionClicked.getY() - i);
           foundBlock = true;
@@ -64,10 +84,7 @@ public class DowsingRodItem extends Item {
         false);
   }
 
-  private boolean isValuableBlock(Block block) {
-    return block == Blocks.COAL_BLOCK
-        || block == Blocks.COPPER_ORE
-        || block == Blocks.DIAMOND_ORE
-        || block == Blocks.IRON_ORE;
+  private boolean isValuableBlock(BlockState block) {
+    return block.isIn(ModTags.Blocks.DOWSING_ROD_DETECTABLE_BLOCKS);
   }
 }
